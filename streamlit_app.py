19f2efd082c237160265cc984d1a4ea246096fb5
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-06-12 13:22:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,32 @@
-# Imports
+
 import streamlit as st
 
 def main():
+    # Imports
     import pandas as pd
     import numpy as np
     from random import random 
     
-    st.set_page_config(page_title = 'Colour Blindness', initial_sidebar_state = 'expanded') 
-
+    # Settings
+    st.set_page_config(page_title = 'Example App', initial_sidebar_state = 'expanded') 
 
     # Title
-    st.title('Title')
+    st.title('Example Streamlit App')
     st.write('Piers Walker 2022')
+    st.write('This is a simple Streamlit App example which demonstrates the application of hosting an App on Streamlit, interfacing with public code hosted on Github')
 
     # Data
+    st.subheader('Set data table size') 
+    num_rows_required = st.number_input('Number of rows')
     st.subheader('Display current data') 
-    data = pd.DataFrame(data = {'Param1':['a', 'b', 'c', 'd'], 'Param2':[10, 15, 20, 25]})
+    data = pd.DataFrame(data = {'Param1':'Param2':np.random.uniform(low=0, high=10, size=num_rows_required).tolist(), 
+                                'Param2':np.random.uniform(low=50, high=100, size=num_rows_required).tolist()})
     st.dataframe(data)
     
-    st.subheader('Select ') 
-    
-    st.slider('Consecutive subset', 0, len(data.index), 1) 
+    #st.subheader('Selections') 
+    #st.slider('Consecutive subset', 0, len(data.index), 1) 
 
-    
+    #submit = st.button('Submit')  
     
 if __name__ == '__main__':
     main()
