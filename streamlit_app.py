153f1a39cc5f403af89265190dcbe052ebcaa763
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-06-12 10:40:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
+# Imports
+import streamlit as st
 import pandas as pd
+import numpy as np
+
+# Data
 df = pd.DataFrame(data = {'Param1':['a', 'b', 'c', 'd'], 'Param2':[10, 15, 20, 25]})
+
 st.write(df)
