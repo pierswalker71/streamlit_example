ca036978df9c2462a3561f7c27a9880cdcad1caa
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-06-12 10:46:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,21 @@
 # Imports
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-# Data
-df = pd.DataFrame(data = {'Param1':['a', 'b', 'c', 'd'], 'Param2':[10, 15, 20, 25]})
+def main():
+    import pandas as pd
+    import numpy as np
+    
+    st.set_page_config(page_title = 'Colour Blindness', initial_sidebar_state = 'expanded') 
 
-st.write(df)
+    # Title
+    st.title('Title')
+    st.write('Piers Walker 2022')
+
+    # Data
+    df = pd.DataFrame(data = {'Param1':['a', 'b', 'c', 'd'], 'Param2':[10, 15, 20, 25]})
+
+    st.write(df)
+    
+if __name__ == '__main__':
+    main()
+    
