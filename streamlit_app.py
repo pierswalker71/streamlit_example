6cd99fc94c11f37a6d173ac27e92dfa80f1f620c
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-06-14 20:11:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,10 @@ def main():
     if submit_append:
         idx = [x for x in range(st.session_state['data'].index.max()+1,st.session_state['data'].index.max()+1+append_num)]
         st.write(f'next idx: {idx}')
-        st.session_state['data'] = pd.concat([st.session_state['data'],
-                                             pd.DataFrame(data = {'Param1':np.random.uniform(low=0, high=10, size=append_num).tolist(), 
-                                                               'Param2':np.random.uniform(low=50, high=100, size=append_num).tolist()},
-                                                           index = idx)])
+        #st.session_state['data'] = pd.concat([st.session_state['data'],
+        #                                     pd.DataFrame(data = {'Param1':np.random.uniform(low=0, high=10, size=append_num).tolist(), 
+        #                                                       'Param2':np.random.uniform(low=50, high=100, size=append_num).tolist()},
+        #                                                   index = idx)])
         #col_current_data.dataframe(st.session_state['data'])
         submit_append = False
     
