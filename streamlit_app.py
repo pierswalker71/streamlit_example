baa899dae98cc2155b1ddbf58b9126f42f7723e2
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-06-18 11:07:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ def main():
     import pandas as pd
     import numpy as np
     from random import random 
-    
+    #=================================================================
 
 
     # Settings
@@ -20,30 +20,26 @@ def main():
     if 'new_rows' not in st.session_state:
         st.session_state['new_rows'] = pd.DataFrame(data = {'Param1':np.random.uniform(low=0, high=10, size=3).tolist(), 
                                         'Param2':np.random.uniform(low=50, high=100, size=3).tolist()})
-    
+        
+    #-----------------------------------------------------------------
+    # Set flags as false. These get set to True on the assocaited streamlit button
     submit_delete = False
     submit_append = False
-    
-    #=================================================================
+   
     # Title
     st.title('Example Streamlit App')
     st.write('Piers Walker 2022. https://github.com/pierswalker71')
-    st.write('This is a simple Streamlit App example. It demonstrates the application of hosting an App on Streamlit interfacing with my public code hosted on Github')
+    st.write('This is a simple Streamlit App example. It demonstrates the application of hosting an App on Streamlit interfacing with code hosted on my Github')
 
     # Data
     st.subheader('Create data table') 
-    #col_current_data = st.columns(1)
     with st.expander('Current data'):
         st.dataframe(st.session_state['data'])
-        #col_current_data.write('test')
-        #col_current_data.dataframe(st.session_state['data'])
                                     
     #=================================================================
     st.subheader('Data Modification') 
     
-    #st.write(f'submit_append: {submit_append}')
-    #st.write(f'submit_delete: {submit_delete}')
-    
+    # Append new rows section
     with st.expander('Append new rows'):
         append_num = st.slider('Number of rows to add to bottom of table', min_value=1, max_value=5, value=1, step=1)
         if len(st.session_state['data'].index)>0:
@@ -62,12 +58,11 @@ def main():
                                
     if submit_append:
         st.session_state['data'] = pd.concat([st.session_state['data'],st.session_state['new_rows']])
-        #st.session_state['data'] = st.session_state['data'].sort_index(inplace=True)
-        #col_current_data.dataframe(st.session_state['data'])
         submit_append = False
         st.experimental_rerun()
     
     #-----------------------------------------------------------------
+    # Delete rows section
     with st.expander('Delete rows'):
         row_ids_to_delete = st.multiselect('Select row IDs to delete',
                                            options=st.session_state['data'].index,
